2a7fb3d5684e0b35d45a633396b73ce8c4285497
ztychr <>
2023-11-13 00:34:22 +0100

    .

--- utils/gen_html.py
+++ utils/gen_html.py
@@ -4,80 +4,88 @@ from random import choice
 from string import ascii_uppercase, ascii_lowercase
 import urllib.parse, os
 
-base_url = 'http://localhost:5000/'
+base_url = 'http://localhost:5000/?'
 #print(url + urllib.parse.urlencode(params))
 
 LANG="DA"
 
-data = {
-    "boing": 1,
-}
+data = {"boing": 1}
 
 layout = {
+    "Root": [
+        "resume.pdf",
+        "resume-new.pdf"
+    ],
+    
     "Pictures": [
-        "p1",
-        "p2",
-        "p3",
-        "p4",
-        "p5"
+        "IMG2622.jpg",
+        "IMG2623.jpg",
+        "IMG2624.jpg"
     ],
+    
     "Documents": [
-        "d1",
-        "d2",
-    ],
+        "meeting-notes.docx",
+        "payslip-october.docx",
+        "budget-for-department.xlsx"
+    ]
 }
 
 def main(LANG, data, layout, base_url):
     for group in data:
         for i in range(data[group]):
             idx = ''.join(choice(ascii_uppercase+ascii_lowercase) for i in range(12))
-            params = {"group": group, "id": idx, "src": "html"}
-            url=base_url + urllib.parse.urlencode(params)
+            params = {"group": group, "id": idx}
         
             for folder in layout:
-                if not os.path.exists("output/%s/%s/%s" % (group, idx, folder)):
-                    os.makedirs("output/%s/%s/%s" % (group, idx, folder))
+                if not os.path.exists("output/%s" % folder):# and not folder == "Root":
+                    os.makedirs("output/%s" % folder)
 
                 for filex in layout[folder]:
-                    if folder == "Pictures":
-                        with open("templates/index.da.html", "r") as f:
-                            data = f.read() 
-                            data = data.replace("REPLACE", url) 
-                
-                        with open("output/%s/%s/%s/%s.jpg.html" % (group, idx, folder, filex), "w") as file: 
-                            file.write(data) 
+                    file_type = filex.rsplit('.', 1)[-1]# for x in la]
+                    if file_type == "jpg":
+                        params["src"] = "html"
+                        url=base_url + urllib.parse.urlencode(params)
+                        make_html(filex, folder, url)
+                    elif file_type == "docx":
+                        params["src"] = "docx"
+                        url=base_url + urllib.parse.urlencode(params)
+                        make_docx(filex, folder, url)
+                    elif file_type == "xlsx":
+                        params["src"] = "xlxs"
+                        url=base_url + urllib.parse.urlencode(params)
+                        make_xlsx(filex, folder, url)
+                    elif file_type == "pdf":
+                        params["src"] = "pdf"
+                        url=base_url + urllib.parse.urlencode(params)
+                        make_html(filex, folder, url)
 
-                        
-#                    print(folder, filex)
+#                        if not os.path.exists("output/%s/%s/%s" % (group, idx, folder)):
+#                            os.makedirs("output/%s/%s/%s" % (group, idx, folder))
 
-#                print(i, layout[i][0])
-            
+def make_html(file_name, path, url):
+    with open("templates/index.da.html", "r") as f:
+        data = f.read() 
+        data = data.replace("REPLACE", url)               
+    with open("output/%s/%s.html" % (path, file_name), "w") as file:
+        file.write(data)
 
+def make_docx(file_name, path, url):
+    with open("output/%s/%s" % (path, file_name), "wb") as f:
+        f.write(
+            make_canary_msword(
+                url=url,
+                template="templates/template.docx",
+            )
+        )
 
-"""
-    for group in data:
-        for i in range(data[group]):
-            idx = ''.join(choice(ascii_uppercase+ascii_lowercase) for i in range(12))
-            if not os.path.exists("output/%s" % idx):
-                os.makedirs("output/%s" % idx)
+def make_xlsx(file_name, path, url):
+    with open("output/%s/%s" % (path, file_name), "wb") as f:
+        f.write(
+            make_canary_msexcel(
+                url=url,
+                template="templates/template.docx",
+            )
+        )
 
-            params = {"group": group, "id": idx}
-            url=base_url + urllib.parse.urlencode(params)
-
-            with open("output/%s/testdoc-%s-%s.docx" % (idx, group, idx), "wb") as f:
-                f.write(
-                    make_canary_msword(
-                        url=url,
-                        template="templates/template.docx",
-                    )
-                )
-            with open("output/%s/testdoc-%s-%s.xlsx" % (idx, group, idx), "wb") as f:
-                f.write(
-                    make_canary_msexcel(
-                        url=url,
-                        template="templates/template.docx",
-                    )
-                )
-"""
 if __name__ == "__main__":
     main(LANG, data, layout, base_url)
